1c05ae94287680a214f90e29bd3e3c8b06c17239
Van Huy <111716590+huyvan2001@users.noreply.github.com>
2023-04-29 02:30:05 +0700

    update

--- database/database.js
+++ database/database.js
@@ -5,6 +5,7 @@ import Exception from '../exceptions/Exception.js'
 mongoose.set('strictQuery', true)
 async function connect() {
     try {
+        console.log(process.env.MONGO_URI)
         let connection = await mongoose.connect(process.env.MONGO_URI)        
         print('Connect mongoose successfully', OutputType.SUCCESS)        
         return connection
